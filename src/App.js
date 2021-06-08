7c1bf97cd63be2a5dfcf5d6f45b2b3a151c771f0
Kevin <kevmunar@hotmail.com>
2021-06-08 18:17:01 -0400

    added space

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +21,35 @@ function App() {
       </header>
     </div>
   );
+
+    <Router>
+
+  <div className="App">
+    
+    <Navbar />
+        
+      <Switch>
+        <Route path="/" exact >
+         <Home  setData ={setData} handleCity = {handleCity} city={city} setSave ={setSave} save={save}/>
+        </Route>
+        <Route path="/weather"> 
+          <Weather  data={data} city={city} />
+         
+        </Route>
+        <Route path="/saved">
+          <Saved  save={save} />
+          
+
+          
+          </Route>  
+        <Route path="/contact" component = {Contact}/>
+        <Route path="/about" component = {About} />
+
+      </Switch>
+  </div>
+  </Router>
+);
+
 }
 
 export default App;
